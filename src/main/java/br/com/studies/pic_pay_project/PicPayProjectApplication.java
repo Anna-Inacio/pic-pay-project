15230221cf70755a58aa38de5137dda5e03b3603
Anna Inacio <anna.inacio@a.fecaf.com.br>
2025-07-20 16:15:12 -0300

    Building Pic-Pay Project

---
+++ src/main/java/br/com/studies/pic_pay_project/PicPayProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.studies.pic_pay_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPayProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPayProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/studies/pic_pay_project/PicPayProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.studies.pic_pay_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicPayProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
