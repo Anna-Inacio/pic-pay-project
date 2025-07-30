386f3ce24592c0eb17f448392ecb932377a673ae
Anna Inacio <anna.inacio@a.fecaf.com.br>
2025-07-30 12:13:50 -0300

    connection config

--- src/main/java/br/com/studies/pic_pay_project/model/BankDetails.java
+++ src/main/java/br/com/studies/pic_pay_project/model/BankDetails.java
@@ -1,11 +1,67 @@
 package br.com.studies.pic_pay_project.model;
 
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "BANK_DETAILS")
 public class BankDetails {
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
     private Integer agency;
     private Integer digit;
     private Integer account;
     private String pixKey;
     private String bankName;
     //TODO FK User
+
+    public Integer getAgency() {
+        return agency;
+    }
+
+    public void setAgency(Integer agency) {
+        this.agency = agency;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Integer getDigit() {
+        return digit;
+    }
+
+    public void setDigit(Integer digit) {
+        this.digit = digit;
+    }
+
+    public Integer getAccount() {
+        return account;
+    }
+
+    public void setAccount(Integer account) {
+        this.account = account;
+    }
+
+    public String getPixKey() {
+        return pixKey;
+    }
+
+    public void setPixKey(String pixKey) {
+        this.pixKey = pixKey;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
 }
